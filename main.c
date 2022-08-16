5efb0f097a160bfaf37820b0408e7332979b8490
kose <53458630+kokseen1@users.noreply.github.com>
2022-08-16 22:58:02 +0800

    Update: Remove pseudo mutex

--- main.c
+++ main.c
@@ -18,7 +18,7 @@
 
 #define STEP_SMALL 0.1
 #define STEP_DEFAULT 3
-#define REPLY_USERDATA_SUB_RELOAD 8000
+
 #define CMD_BUF_MAX 1024
 #define SMALL_BUF_MAX 32
 #define DEFAULT_SUB_FNAME "out.srt"
@@ -38,7 +38,6 @@ static double ts_s_d;
 
 static char cmd_buf[CMD_BUF_MAX];
 
-static int mtx_reload = 0;
 static int insert_mode = 0;
 
 static char *sub_fname = NULL;
@@ -159,8 +158,7 @@ static void frame_back_step()
 static void reload_sub()
 {
     const char *cmd[] = {"sub-reload", NULL};
-    mtx_reload++;
-    mpv_command_async(mpv, REPLY_USERDATA_SUB_RELOAD, cmd);
+    mpv_command_async(mpv, 0, cmd);
 }
 
 static void clear_cmd_buf()
@@ -197,12 +195,6 @@ static void add_sub(char *fname)
 
 static void export_sub()
 {
-    if (mtx_reload)
-    {
-        // Don't touch file if reload is already in progress
-        return;
-    }
-
     FILE *pFile = fopen(DEFAULT_SUB_FNAME, "w");
 
     if (sub_head)
@@ -839,13 +831,6 @@ int main(int argc, char *argv[])
                     if (mp_event->event_id == MPV_EVENT_FILE_LOADED)
                     {
                         init();
-                    }
-                    if (mp_event->event_id == MPV_EVENT_COMMAND_REPLY)
-                    {
-                        if (mp_event->reply_userdata == REPLY_USERDATA_SUB_RELOAD)
-                        {
-                            mtx_reload--;
-                        }
                     }
                     if (mp_event->event_id == MPV_EVENT_GET_PROPERTY_REPLY)
                     {
